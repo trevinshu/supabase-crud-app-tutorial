da8e0b4a737253c854a5cd45de1c6ab68d0f7774
Trevin Shu <trevinshu2008@gmail.com>
2022-10-10 02:44:19 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Supabase Crash Course
+
+## Source
+
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9hUb6sHthUEwG7r9VDPBMKO
---
+++ src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import supabase from '../config/supabaseClient';
+import { useEffect, useState } from 'react';
+import SmoothieCard from '../components/SmoothieCard';
+
+const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState('created_at');
+
+  const handleDelete = (id) => {
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((sm) => sm.id !== id);
+    });
+  };
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from('smoothies').select().order(orderBy, { ascending: false });
+
+      if (error) {
+        setFetchError('Could not fetch the Smoothies');
+        setSmoothies(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, [orderBy]);
+
+  return (
+    <div className="page home">
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            {orderBy}
+          </div>
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
